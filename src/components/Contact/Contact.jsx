1a8b58bf9753b370e04f91cd2043c31498aa1212
inezkaj <ienzkajed@icloud.com>
2024-03-03 16:39:09 +0100

    feat: phonebook07

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,28 @@
+import { useDispatch } from 'react-redux';
+import { removeContact } from '../../redux/tasksSlice';
+import css from './Contact.module.css';
+import PropTypes from 'prop-types';
+
+const Contact = ({ contact }) => {
+  const dispatch = useDispatch();
+
+  const handleRemove = () => {
+    console.log(`${contact.id}`);
+    dispatch(removeContact(contact.id));
+  };
+
+  return (
+    <li key={contact.id}>
+      <span>{contact.name}</span>:&nbsp;
+      <span>{contact.phone}</span>
+      <button className={css.btnDelete} onClick={handleRemove}>
+        delete
+      </button>
+    </li>
+  );
+};
+
+Contact.propTypes = {
+  contact: PropTypes.object,
+};
+export default Contact;
